35f088d81d86195c619c847f4114da74765ddf04
Bima Surya Nurwahid <bimasurya729@gmail.com>
2023-05-24 20:22:49 +0700

    font-style global

---
+++ lib/constant/textstyle_constant.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyleConst {
+  TextStyle heading1 = GoogleFonts.expletusSans(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xFF030F51),
+  );
+  TextStyle heading2 = GoogleFonts.expletusSans(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xFF030F51),
+  );
+  TextStyle heading3 = GoogleFonts.expletusSans(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xFF030F51),
+  );
+  TextStyle heading4 = GoogleFonts.expletusSans(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xFF030F51),
+  );
+  TextStyle heading5 = GoogleFonts.expletusSans(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xFF030F51),
+  );
+
+  TextStyle description1 = GoogleFonts.ptSans(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+  TextStyle description2 = GoogleFonts.ptSans(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+  TextStyle description3 = GoogleFonts.ptSans(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+  TextStyle description4 = GoogleFonts.ptSans(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+}
