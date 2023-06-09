07b7ca098b8562f23829d58117f545a7a6f707e0
Monkey D <rhlbandoe@gmail.com>
2023-06-09 19:48:52 +0800

    update feature credit data

--- lib/constant/provider_icon_constant.dart
+++ lib/constant/provider_icon_constant.dart
@@ -16,6 +16,24 @@ Image? providerIcons(String phoneNumber) {
       scale: 4,
     );
   }
+  if (phoneNumber.substring(2, 3) == "1" &&
+          phoneNumber.substring(3, 4) == "7" ||
+      phoneNumber.substring(2, 3) == "1" &&
+          phoneNumber.substring(3, 4) == "8" ||
+      phoneNumber.substring(2, 3) == "1" &&
+          phoneNumber.substring(3, 4) == "9" ||
+      phoneNumber.substring(2, 3) == "5" &&
+          phoneNumber.substring(3, 4) == "9" ||
+      phoneNumber.substring(2, 3) == "7" &&
+          phoneNumber.substring(3, 4) == "7" ||
+      phoneNumber.substring(2, 3) == "7" &&
+          phoneNumber.substring(3, 4) == "8") {
+    useSuffix;
+    return Image.asset(
+      "assets/icons/provider_xl_icon.png",
+      scale: 4,
+    );
+  }
   useSuffix = false;
   return null;
 }
