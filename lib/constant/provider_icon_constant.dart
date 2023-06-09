297b55db30b0331acaf144fa4ae3db2e3c9e6013
Monkey D <rhlbandoe@gmail.com>
2023-06-09 20:10:10 +0800

    update feature credit data

--- lib/constant/provider_icon_constant.dart
+++ lib/constant/provider_icon_constant.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 Image? providerIcons(String phoneNumber) {
   bool useSuffix = true;
-  if (phoneNumber.substring(2, 3) == "3" &&
-          phoneNumber.substring(3, 4) == "1" ||
+  if (phoneNumber.substring(2, 3) == "3" && phoneNumber.substring(3, 4) == "1" ||
       phoneNumber.substring(2, 3) == "3" &&
           phoneNumber.substring(3, 4) == "2" ||
       phoneNumber.substring(2, 3) == "3" &&
@@ -15,8 +14,7 @@ Image? providerIcons(String phoneNumber) {
       "assets/icons/provider_axis_icon.png",
       scale: 4,
     );
-  }
-  if (phoneNumber.substring(2, 3) == "1" &&
+  } else if (phoneNumber.substring(2, 3) == "1" &&
           phoneNumber.substring(3, 4) == "7" ||
       phoneNumber.substring(2, 3) == "1" &&
           phoneNumber.substring(3, 4) == "8" ||
@@ -31,6 +29,67 @@ Image? providerIcons(String phoneNumber) {
     useSuffix;
     return Image.asset(
       "assets/icons/provider_xl_icon.png",
+      scale: 4.5,
+    );
+  } else if (phoneNumber.substring(2, 3) == "1" &&
+          phoneNumber.substring(3, 4) == "1" ||
+      phoneNumber.substring(2, 3) == "1" &&
+          phoneNumber.substring(3, 4) == "2" ||
+      phoneNumber.substring(2, 3) == "1" &&
+          phoneNumber.substring(3, 4) == "3" ||
+      phoneNumber.substring(2, 3) == "2" &&
+          phoneNumber.substring(3, 4) == "1" ||
+      phoneNumber.substring(2, 3) == "2" &&
+          phoneNumber.substring(3, 4) == "2" ||
+      phoneNumber.substring(2, 3) == "2" &&
+          phoneNumber.substring(3, 4) == "3" ||
+      phoneNumber.substring(2, 3) == "5" &&
+          phoneNumber.substring(3, 4) == "1" ||
+      phoneNumber.substring(2, 3) == "5" &&
+          phoneNumber.substring(3, 4) == "2" ||
+      phoneNumber.substring(2, 3) == "5" &&
+          phoneNumber.substring(3, 4) == "3") {
+    useSuffix;
+    return Image.asset(
+      "assets/icons/provider_tsel_icon.png",
+      scale: 4,
+    );
+  } else if (phoneNumber.substring(2, 3) == "9" &&
+          phoneNumber.substring(3, 4) == "5" ||
+      phoneNumber.substring(2, 3) == "9" &&
+          phoneNumber.substring(3, 4) == "6" ||
+      phoneNumber.substring(2, 3) == "9" &&
+          phoneNumber.substring(3, 4) == "7" ||
+      phoneNumber.substring(2, 3) == "9" &&
+          phoneNumber.substring(3, 4) == "8" ||
+      phoneNumber.substring(2, 3) == "9" &&
+          phoneNumber.substring(3, 4) == "9") {
+    useSuffix;
+    return Image.asset(
+      "assets/icons/provider_tri_icon.png",
+      scale: 4,
+    );
+  } else if (phoneNumber.substring(2, 3) == "8" &&
+          phoneNumber.substring(3, 4) == "1" ||
+      phoneNumber.substring(2, 3) == "8" &&
+          phoneNumber.substring(3, 4) == "2" ||
+      phoneNumber.substring(2, 3) == "8" &&
+          phoneNumber.substring(3, 4) == "3" ||
+      phoneNumber.substring(2, 3) == "8" &&
+          phoneNumber.substring(3, 4) == "4" ||
+      phoneNumber.substring(2, 3) == "8" &&
+          phoneNumber.substring(3, 4) == "5" ||
+      phoneNumber.substring(2, 3) == "8" &&
+          phoneNumber.substring(3, 4) == "6" ||
+      phoneNumber.substring(2, 3) == "8" &&
+          phoneNumber.substring(3, 4) == "7" ||
+      phoneNumber.substring(2, 3) == "8" &&
+          phoneNumber.substring(3, 4) == "8" ||
+      phoneNumber.substring(2, 3) == "8" &&
+          phoneNumber.substring(3, 4) == "9") {
+    useSuffix;
+    return Image.asset(
+      "assets/icons/provider_smartfren_icon.png",
       scale: 4,
     );
   }
