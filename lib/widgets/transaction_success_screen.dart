0ad492595401d29aca372355067164c6e1441109
Bima Surya Nurwahid <bimasurya729@gmail.com>
2023-06-18 18:07:34 +0700

    nambah willpopscope pada transaction succes

--- lib/widgets/transaction_success_screen.dart
+++ lib/widgets/transaction_success_screen.dart
@@ -14,160 +14,164 @@ class TransactionSuccesScreen extends StatelessWidget {
     DateTime now = DateTime.now().toLocal();
     String formattedDate = DateFormat("dd/MM/yyyy | HH:mm:ss").format(now);
 
-    return Container(
-      color: Colors.white,
-      width: 360,
-      height: MediaQuery.of(context).size.height,
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 96),
-            Image.asset(
-              "assets/icons/check_icon.png",
-              width: 58,
-              height: 58,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 13, bottom: 30),
-              child: Text(
-                "Transaction Success",
-                style: TextStyleConst.heading4,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Container(
+        color: Colors.white,
+        width: 360,
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 96),
+              Image.asset(
+                "assets/icons/check_icon.png",
+                width: 58,
+                height: 58,
               ),
-            ),
-            Container(
-              width: 234,
-              height: 381,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color(0xff000000).withOpacity(0.25),
-                    spreadRadius: 2,
-                    blurRadius: 8,
-                    offset: const Offset(2, 3),
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.only(top: 13, bottom: 30),
+                child: Text(
+                  "Transaction Success",
+                  style: TextStyleConst.heading4,
+                ),
               ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 27, bottom: 5),
-                    child: Text(
-                      "Date",
-                      style: TextStyleConst.heading5WithColor(
+              Container(
+                width: 234,
+                height: 381,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xff000000).withOpacity(0.25),
+                      spreadRadius: 2,
+                      blurRadius: 8,
+                      offset: const Offset(2, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 27, bottom: 5),
+                      child: Text(
+                        "Date",
+                        style: TextStyleConst.heading5WithColor(
+                          const Color(0xff1d1d1d),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      formattedDate,
+                      style: TextStyleConst.description4WithColor(
                         const Color(0xff1d1d1d),
                       ),
                     ),
-                  ),
-                  Text(
-                    formattedDate,
-                    style: TextStyleConst.description4WithColor(
-                      const Color(0xff1d1d1d),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 5),
+                      child: Text(
+                        "Product",
+                        style: TextStyleConst.heading5WithColor(
+                          const Color(0xff1d1d1d),
+                        ),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 5),
-                    child: Text(
-                      "Product",
-                      style: TextStyleConst.heading5WithColor(
+                    Text(
+                      "Credit Axis",
+                      style: TextStyleConst.description4WithColor(
                         const Color(0xff1d1d1d),
                       ),
                     ),
-                  ),
-                  Text(
-                    "Credit Axis",
-                    style: TextStyleConst.description4WithColor(
-                      const Color(0xff1d1d1d),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 5),
+                      child: Text(
+                        "Payment Method",
+                        style: TextStyleConst.heading5WithColor(
+                          const Color(0xff1d1d1d),
+                        ),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 5),
-                    child: Text(
-                      "Payment Method",
-                      style: TextStyleConst.heading5WithColor(
+                    Text(
+                      "Transfer Bank",
+                      style: TextStyleConst.description4WithColor(
                         const Color(0xff1d1d1d),
                       ),
                     ),
-                  ),
-                  Text(
-                    "Transfer Bank",
-                    style: TextStyleConst.description4WithColor(
-                      const Color(0xff1d1d1d),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 5),
+                      child: Text(
+                        "Total",
+                        style: TextStyleConst.heading5WithColor(
+                          const Color(0xff1d1d1d),
+                        ),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 5),
-                    child: Text(
-                      "Total",
-                      style: TextStyleConst.heading5WithColor(
+                    Text(
+                      "Rp. 7.000,00",
+                      style: TextStyleConst.description4WithColor(
                         const Color(0xff1d1d1d),
                       ),
                     ),
-                  ),
-                  Text(
-                    "Rp. 7.000,00",
-                    style: TextStyleConst.description4WithColor(
-                      const Color(0xff1d1d1d),
+                    const SizedBox(height: 44),
+                    Image.asset(
+                      "assets/icons/medal_icon.png",
+                      width: 23,
+                      height: 23,
                     ),
-                  ),
-                  const SizedBox(height: 44),
-                  Image.asset(
-                    "assets/icons/medal_icon.png",
-                    width: 23,
-                    height: 23,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 3, bottom: 3),
-                    child: Text(
-                      "Congratulation!",
-                      style: TextStyleConst.heading4,
+                    Padding(
+                      padding: const EdgeInsets.only(top: 3, bottom: 3),
+                      child: Text(
+                        "Congratulation!",
+                        style: TextStyleConst.heading4,
+                      ),
                     ),
-                  ),
-                  Text(
-                    "You get 7 tPoint",
-                    style: TextStyleConst.description3WithColor(
-                      const Color(0xffB26801),
+                    Text(
+                      "You get 7 tPoint",
+                      style: TextStyleConst.description3WithColor(
+                        const Color(0xffB26801),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 34, bottom: 10),
-              child: ButtonCustome(
+              Padding(
+                padding: const EdgeInsets.only(top: 34, bottom: 10),
+                child: ButtonCustome(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const GetRewardScreen(),
+                      ),
+                    );
+                  },
+                  title: "Detail Point",
+                  backgroundColour: const Color(0xff347C17),
+                  width: 234,
+                  style: TextStyleConst.description3WithColor(Colors.white),
+                ),
+              ),
+              ButtonCustome(
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const GetRewardScreen(),
-                    ),
-                  );
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            const BottomNavBar(currentIndex: 0),
+                      ),
+                      (route) => false);
                 },
-                title: "Detail Point",
-                backgroundColour: const Color(0xff347C17),
+                title: "Done",
+                backgroundColour: const Color(0xff030F51),
                 width: 234,
                 style: TextStyleConst.description3WithColor(Colors.white),
               ),
-            ),
-            ButtonCustome(
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const BottomNavBar(currentIndex: 0),
-                    ),
-                    (route) => false);
-              },
-              title: "Done",
-              backgroundColour: const Color(0xff030F51),
-              width: 234,
-              style: TextStyleConst.description3WithColor(Colors.white),
-            ),
-            const SizedBox(
-              height: 96,
-            ),
-          ],
+              const SizedBox(
+                height: 96,
+              ),
+            ],
+          ),
         ),
       ),
     );
